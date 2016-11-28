b1805f5ce9a05fcb6bf54ef650ad834e5b17dae9
peterprib <peter.prib@gmail.com>
2016-11-28 12:07:56 +1100

    Update XslTransform.js

--- XslTransform.js
+++ XslTransform.js
@@ -84,3 +84,12 @@ XslTransform.prototype.transformEngineMoz = function () {
 XslTransform.prototype.getText = function () {
 		return this.output.firstChild.nextSibling.innerText;;
 	};
+
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module);
+}
+define(function(require) {
+	    //The value returned from the function is
+	    //used as the module export visible to Node.
+	    return XslTransform;
+	});
